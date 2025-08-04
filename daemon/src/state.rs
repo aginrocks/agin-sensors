db93739d1a38fb6c9490750d0bbd8b4e4e528def
Tymek <vonautymek@gmail.com>
2025-08-04 12:10:25 +0200

    daemon: added creating connectors

--- daemon/src/state.rs
+++ daemon/src/state.rs
@@ -1,5 +1,6 @@
 use color_eyre::eyre::Result;
-use modules::{database::IntoGlobalDB, databases::GlobalDB};
+use modules::connector::ConnectorBuilder;
+use modules::{connectors::ConnectorType, database::IntoGlobalDB, databases::GlobalDB};
 use std::{collections::HashMap, sync::Arc};
 use tokio::{fs::read_to_string, sync::OnceCell};
 
@@ -7,6 +8,7 @@ use crate::global_config::GlobalConfig;
 
 pub struct AppState {
     pub databases: HashMap<String, GlobalDB>,
+    pub connectors: HashMap<String, ConnectorType>,
 }
 
 impl AppState {
@@ -22,7 +24,18 @@ impl AppState {
             .map(|(db_name, db_config)| (db_name, db_config.into_global_db()))
             .collect::<HashMap<_, _>>();
 
-        Ok(Arc::new(AppState { databases }))
+        let connectors = parsed_config
+            .connectors
+            .into_iter()
+            .map(|(connector_name, connector_config)| {
+                (connector_name, ConnectorType::new(&connector_config))
+            })
+            .collect::<HashMap<_, _>>();
+
+        Ok(Arc::new(AppState {
+            databases,
+            connectors,
+        }))
     }
 }
 
