49cc60f28fca912df447c72204469b28a051568d
Piernik <ziernikmichal@gmail.com>
2025-08-10 16:57:49 +0200

    modifier template: build example modifier which multiplies values by 2

--- modifier_template/src/lib.rs
+++ modifier_template/src/lib.rs
@@ -1,12 +1,26 @@
 use aginsensors_core::{connector::Measurement, define_modifier, modifier::Modifier};
 use color_eyre::eyre::Result;
+use serde::de::value;
 
 define_modifier!("modifier_template", ModifierTemplate);
 
 impl Modifier for ModifierTemplate {
     fn calc(&self, measurements: Vec<Measurement>) -> Result<Vec<Measurement>> {
         // Implement the calculation logic here
-        tracing::info!("Calculating with ModifierTemplate for measurements:",);
-        Ok(vec![measurements[0].clone()]) // Placeholder return value
+        tracing::info!(
+            "Calculating with ModifierTemplate for measurements: {:?}",
+            measurements
+        );
+
+        let mut res = measurements.clone();
+        res.iter_mut().for_each(|measurement| {
+            measurement.values.iter_mut().for_each(|(_key, value)| {
+                *value *= 2.0;
+            });
+        });
+
+        tracing::info!("Calculated: {:?}", res);
+
+        Ok(res)
     }
 }
