886d2c32221ae0398f32674647d7c044fedeeb34
Piernik <ziernikmichal@gmail.com>
2025-10-01 22:45:30 +0200

    refresh

--- landing_page/lib/config.ts
+++ landing_page/lib/config.ts
@@ -1,3 +1,3 @@
 export const repoUrl = "https://github.com/aginrocks/agin-sensors";
 export const demoUrl =
-  "https://demo.sensors.agin.rocks/d/adkgbd9/demo?orgId=1&kiosk=true";
+  "https://demo.sensors.agin.rocks/d/adkgbd9/demo?orgId=1&kiosk=true&refresh=5s";
